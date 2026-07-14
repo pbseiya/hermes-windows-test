484f6cfcfa2f6b64353aa9b5e952ef518fc9fa4f
Pongsak Boonpalit <34887773+pbseiya@users.noreply.github.com>
2026-07-14 12:04:16 +0700

    fix: suppress pip dependency warning (use Start-Process with RedirectStandardError)

--- quick-install.ps1
+++ quick-install.ps1
@@ -314,7 +314,7 @@ if ($pythonIsValid) {
         $getPipUrl = 'https://bootstrap.pypa.io/get-pip.py'
         $getPipFile = Join-Path $pythonDir 'get-pip.py'
         Invoke-WebRequest -Uri $getPipUrl -OutFile $getPipFile -UseBasicParsing
-        & $pythonExe $getPipFile 2>$null
+        Start-Process -FilePath $pythonExe -ArgumentList $getPipFile -Wait -NoNewWindow -RedirectStandardError $null
         Write-Ok 'pip installed'
     }
 }
