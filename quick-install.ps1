1f13ff0827f56fa4909469ba0180527b885a2c0b
Pongsak Boonpalit <pongsak.b@irpc.co.th>
2026-08-04 16:57:46 +0700

    Fix git clone stderr error that causes script to exit prematurely

--- quick-install.ps1
+++ quick-install.ps1
@@ -464,7 +464,7 @@ if (-not $SkipInstall) {
                 Write-Err "Cannot remove old hermes directory: $hermesInstallDir`nPlease close any running hermes processes and try again, or remove manually:`n  Remove-Item '$hermesInstallDir' -Recurse -Force"
             }
 
-            git clone --depth 1 https://github.com/NousResearch/hermes-agent.git $hermesInstallDir
+            git clone --depth 1 https://github.com/NousResearch/hermes-agent.git $hermesInstallDir 2>$null
         }
     }
     else {
@@ -473,7 +473,7 @@ if (-not $SkipInstall) {
         if (-not (Test-Path $hermesParentDir)) {
             New-Item -ItemType Directory -Path $hermesParentDir -Force | Out-Null
         }
-        git clone --depth 1 https://github.com/NousResearch/hermes-agent.git $hermesInstallDir
+        git clone --depth 1 https://github.com/NousResearch/hermes-agent.git $hermesInstallDir 2>$null
     }
 
     # Validate clone succeeded
